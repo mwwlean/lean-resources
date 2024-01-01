6c1c5d3c54af5410959caa970494a8480703a9d3
mwwlean <foolsparadise984@gmail.com>
2024-01-02 02:54:50 +0800

    tuesday - aboutpage

--- dd-tools/src/routes/components/footer.svelte
+++ dd-tools/src/routes/components/footer.svelte
@@ -1,8 +1,9 @@
 <script>
     import { Footer, FooterCopyright, FooterLinkGroup, FooterBrand, FooterLink } from 'flowbite-svelte';
   </script>
-  
+ 
     <Footer>
         <div class="flex justify-center m-auto">
     <p class="text-xs  dark:text-slate-50 tracking-tight">Created with SvelteKit and TailwindCSS by <span><a class="text-orange-400 font-bold underline" href="https://github.com/mwwlean">mwwlean@github 🤠</a></span></p></div>
-    </Footer>
+  </Footer>
+ 
