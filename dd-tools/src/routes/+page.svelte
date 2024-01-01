1e32dd92520516042be461ee2ec1f1111dd899e4
mwwlean <foolsparadise984@gmail.com>
2024-01-01 21:23:56 +0800

    Monday - Svelte Setup & navbar

---
+++ dd-tools/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+ 
+ <h1>Home Pages</h1>
+ <p>Welcome to the home page!</p>
+
+ <style lang="postcss">
+  :global(html) {
+   background-color: theme(colors.gray.100);
+  }
+ </style>
+ 
---
+++ dd-tools/src/routes/components/navbar.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+  import { DarkMode } from 'flowbite-svelte';
+
+  let btnClass = 'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg text-xl p-2 md:p-0';
+</script>
+
+<Navbar class="bg-slate-200">
+  <NavBrand href="/">
+    <span class="self-center whitespace-nowrap text-xl font-bold dark:text-white ">Devtools</span>
+  </NavBrand>
+  <NavHamburger />
+  <NavUl>
+    <NavLi href="/about">About</NavLi>
+    <NavLi href="/appdev">App</NavLi>
+    <NavLi href="/pricing">Pricing</NavLi>
+    <NavLi href="/contact">Contact</NavLi>
+    <DarkMode {btnClass} />
+  </NavUl>
+</Navbar>
+
+<style>
+
+</style>
