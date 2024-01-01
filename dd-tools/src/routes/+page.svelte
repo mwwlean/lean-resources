bcdf828ad2d8f7a2184375655797edcb9f764ee9
mwwlean <foolsparadise984@gmail.com>
2024-01-02 02:41:59 +0800

    Tuesday - Done hero page

--- dd-tools/src/routes/+page.svelte
+++ dd-tools/src/routes/+page.svelte
@@ -1,10 +1,20 @@
- 
- <h1>Home Pages</h1>
- <p>Welcome to the home page!</p>
+<script>
+     import Footer from "./components/footer.svelte";
+</script>
+
+<main style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 80vh;">
+    <div class="flex justify-center flex-row justify-center items-center content-center max-w-screen-md flex-col m-auto text-center">
+        <div class="m-auto p-5">
+            <p class="md:text-xl text-lg pt-5 text-orange-400 tracking-tight">Your Easy Path to Coding Resources💻</p>
+            <p class="md:text-6xl text-5xl font-bold dark:text-slate-50 pt-2 tracking-tight">DEV EXPLORER</p>
+            <p class="md:text-lg text-base font-normal pt-5 dark:text-slate-50 tracking-tight">Simplify your coding journey with <span class="font-semibold">Dev Explorer</span>. All the resources you need, neatly listed for you. Tutorials, tools, and more - discover it all effortlessly.👌🏼</p>
+            <a href="/about"><button class="bg-orange-500 p-2 pl-5 pr-5 hover:scale-[1.2] cursor-pointer text-white mt-6 rounded-lg font-semibold">Get Started 🚀</button></a>
+        </div>
+    </div>
+</main>
+<Footer></Footer>
 
  <style lang="postcss">
-  :global(html) {
-   background-color: theme(colors.gray.100);
-  }
+ 
  </style>
  
--- dd-tools/src/routes/components/navbar.svelte
+++ dd-tools/src/routes/components/navbar.svelte
@@ -5,20 +5,16 @@
   let btnClass = 'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg text-xl p-2 md:p-0';
 </script>
 
-<Navbar class="bg-slate-200">
+<Navbar class="bg-slate-100 dark:bg-gray-800 p-4 dark:border-slate-600 border-b border-slate-400">
   <NavBrand href="/">
-    <span class="self-center whitespace-nowrap text-xl font-bold dark:text-white ">Devtools</span>
+    <span class="self-center whitespace-nowrap text-2xl font-bold text-orange-400 ">DDtools</span>
   </NavBrand>
   <NavHamburger />
-  <NavUl>
-    <NavLi href="/about">About</NavLi>
-    <NavLi href="/appdev">App</NavLi>
-    <NavLi href="/pricing">Pricing</NavLi>
-    <NavLi href="/contact">Contact</NavLi>
-    <DarkMode {btnClass} />
+    <NavUl>
+      <NavLi href="/about" class="lg:text-base font-normal">About</NavLi>
+      <NavLi href="/appdev" class="lg:text-base font-normal">Design Tools</NavLi>
+      <NavLi href="/pricing" class="lg:text-base font-normal">Web Development Tools</NavLi>
+      <NavLi href="/contact" class="lg:text-base font-normal">Mobile Development Tools</NavLi>
+    <DarkMode {btnClass}/>
   </NavUl>
 </Navbar>
-
-<style>
-
-</style>
