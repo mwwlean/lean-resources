8eef86d61334ea3efcc3de967dd904906832cfca
leander <122543845+mwwlean@users.noreply.github.com>
2024-01-06 02:06:07 +0800

    Update +page.svelte

--- dd-tools/src/routes/+page.svelte
+++ dd-tools/src/routes/+page.svelte
@@ -8,7 +8,7 @@
             <p class="md:text-lg text-xs pt-5 text-orange-400 tracking-tight">Your Easy Path to Coding Resources💻</p>
             <p class="md:text-6xl text-4xl font-black dark:text-slate-100 pt-2 tracking-tight text-gray-950">LEAN RESOURCES</p>
             <p class="md:text-lg text-base font-light pt-3 dark:text-slate-100 text-gray-700">All the resources you need, neatly listed for you. Tutorials, tools, and more - discover it all effortlessly.👌🏼</p>
-            <a href="/design-pages"><button class="bg-orange-500 p-2 pl-5 pr-5 hover:scale-[1.2] cursor-pointer text-white mt-6 rounded-lg font-semibold border border-orange-400 hover:border-slate-400">Get started 🚀</button></a>
+            <a href="/design"><button class="bg-orange-500 p-2 pl-5 pr-5 hover:scale-[1.2] cursor-pointer text-white mt-6 rounded-lg font-semibold border border-orange-400 hover:border-slate-400">Get started 🚀</button></a>
         </div>
     </div>
 </main>
